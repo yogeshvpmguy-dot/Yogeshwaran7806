40ba83b664ec035c9ecd31fa45cc34295e5e6a87
yogeshvpmguy-dot <yogeshvpmguy@gmail.com>
2025-08-30 23:22:37 +0530

    Add files via upload

---
+++ 20250830-231502.NM.cpp
@@ -0,0 +1,25 @@
+<textarea name="message" placeholder="Your Message" rows="5" required></textarea>
+
+        <button type="submit" class="btn">Send Message</button>
+
+        <p id="formStatus"></p>
+
+      </form>
+
+    </div>
+
+  </section>
+
+  <!-- Footer -->
+
+  <footer class="footer">
+
+    <p>© <span id="year"></span> Yogeshwaran | Made with 💖</p>
+
+  </footer>
+
+  <script src="script.js"></script>
+
+</body>
+
+</html>
